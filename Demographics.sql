491ccc518c52aca320feecec321738fdac6e1885
Andrewsr220a <Raish.a.andrews@gmail.com>
2021-12-29 12:43:33 -0500

    Changing name of dataset, Adding sql query

---
+++ Demographics.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Demographics_summary (
+    Job_Category CHAR(40),
+    Year DATE,
+    Month INT,
+    Date DATE,
+    Sex CHAR,
+    White__not_Hispanic___Latino_ INT,
+    African_American__not_Hispanic_ INT,
+    American_Indian___Alaskan_Nativ INT,
+    Asian INT   NOT NULL,
+    Native_Hawaiian_or_Other_Pacifi INT,
+    Two_or_More_Races INT,
+    Other INT,
+    ObjectId INT);
